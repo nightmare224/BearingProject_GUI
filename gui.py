d3916da4690d0d67defc97460a8eeb866ea11f53
nightmare224 <nicole1999224@gmail.com>
2020-09-07 14:15:41 +0800

    the backup before seperater this file

--- gui.py
+++ gui.py
@@ -1,9 +1,19 @@
 import dash
 import dash_core_components as dcc
+import dash_daq as daq
 import dash_html_components as html
+
 import plotly.express as px
 import pandas as pd
 import numpy as np
+import time
+import os
+
+# outlier detect
+from sklearn.ensemble import IsolationForest
+
+# add in persistence=True dcc to prevent refresh while switching https://dash.plotly.com/persistence
+
 
 external_stylesheets = ['https://codepen.io/amyoshino/pen/jzXypZ.css','https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -13,7 +23,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # see https://plotly.com/python/px-arguments/ for more options
 
 
-mainfile = np.load('/Users/nicole/Downloads/Formal/Cage_Break.npy').reshape(-1)
+mainfile = np.load('./Formal/Cage_Break.npy').reshape(-1)
 fig1 = px.line(mainfile, title = 'Frequency Domain')
 fig2 = px.line(mainfile, title = 'Envolope')
 # fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
@@ -39,7 +49,17 @@ page1 =\
                         'font-weight' : 'bold'
                     }
                 ),
-                html.Div(children = [html.Li('Filename : ball.npy'), html.Li('Data Size : 1.6 MB'), html.Li('Date : July 30, 2020 at 10:37 PM')],
+                html.Div(id = 'fileinfo', children = [
+                    dcc.Markdown('''
+                        #### Dash and Markdown
+
+                        Dash supports [Markdown](http://commonmark.org/help).
+
+                        Markdown is a simple way to write and format text.
+                        It includes a syntax for things like **bold text** and *italics*,
+                        [links](http://commonmark.org/help), inline `code` snippets, lists,
+                        quotes, and more.
+                    ''')],
                     style = {
                         'position' : 'relative',
                         'top' : '60px',
@@ -50,6 +70,8 @@ page1 =\
                         # 'text-align' : 'center'
                     }
                 ),
+
+
                 html.Button('Load', 
                     id='button', 
                     style = {
@@ -60,20 +82,33 @@ page1 =\
                         'height' : '80px'
                     }
                 ),
-                dcc.RadioItems(
-                    options=[
-                        {'label': 'No Filter', 'value': '0'},
-                        {'label': 'With Filter', 'value': '1'}
-                    ],
-                    value='0',
-                    labelStyle={'display': 'inline-block'},
+                # dcc.RadioItems(
+                #     options=[
+                #         {'label': 'Origianl', 'value': '0'},
+                #         {'label': 'Drop Outlier', 'value': '1'}
+                #     ],
+                #     value='0',
+                #     labelStyle={'display': 'inline-block'},
+                #     style = {
+                #         'position' : 'relative',
+                #         'top' : '30px',
+                #         'left' : '10px'
+                #     }
+                # )
+                # relative : compare with original position
+                daq.BooleanSwitch(
+                    id='outlier_detect',
+                    label = 'Enable Outlier Detection',
+                    labelPosition = 'top',
+                    on=False,
                     style = {
                         'position' : 'relative',
-                        'top' : '30px',
-                        'left' : '10px'
+                        'top' : '0px',
+                        'right' : '50px'
                     }
                 )
-                ],
+
+            ],
                 
                 style = {
                     # 'borderStyle' : 'dashed',
@@ -114,12 +149,13 @@ page1 =\
                     'position' : 'relative',
                     'top' : '40px',
                     # 'background-color' : '#FFD382',
-                }
+                },
+                persistence=True
         
             )],
             className = 'row',
-        )
-    ])
+        )]
+    )
 
 page2 =\
     html.Div(id = 'page2', children = [
@@ -204,41 +240,52 @@ from dash.dependencies import Output, Input, State
     [
         Output(component_id = 'fig1', component_property = 'figure'),
         Output(component_id = 'fig2', component_property = 'figure'),
-        Output(component_id = 'fig3', component_property = 'figure')],
+        Output(component_id = 'fig3', component_property = 'figure'),
+        Output(component_id = 'fileinfo', component_property = 'children')],
     [Input(component_id = 'funcSelect', component_property = 'value'), Input('bearing_file_upload', 'contents')],
     [State('bearing_file_upload', 'filename'), State('bearing_file_upload', 'last_modified')])
 def upload_file(opt, contents, filename, filedates):
     global mainfile
     if filename is None:
-        return {},{},{}
+        return {},{},{},[]
     elif filename.endswith('.npy'):
-        sourcefile = np.load('/Users/nicole/Downloads/Formal/'+filename)
+        sourcefile = np.load('./Formal/'+filename)
         print(sourcefile.shape)
     elif filename.endswith('.csv'):
         sourcefile = pd.read_csv(filename)
     elif filename.endswith('.xlsx'):
         sourcefile = pd.read_excel(filename)
     else:
-        # print('Filetype Not Support')
-        print('filename = ',filename)
-        print('option = ', opt)
+        print('Filetype Not Support')
+        # print('filename = ',filename)
+        # print('option = ', opt)
         return None,
     
     mainfile = np.array(sourcefile).reshape(-1)
+    # always outlier detect
+    mainfile = outlier_detect(mainfile)
 
     # fig = drawGraph(opt)
     fig1 = drawGraph(0)
     fig2 = drawGraph(1)
     fig3 = drawGraph(2)
 
-    return fig1, fig2, fig3
+    fileinfo = [html.Li('Filename : {}'.format(filename)), 
+                html.Li('Last Modified : {}'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(filedates)))), 
+                html.Li('Data Size : {:.2f} MB'.format(os.stat('./Formal/'+filename)[-4]/1024/1024))]
+
+
+
+    # print(len(contents))
+
+    return fig1, fig2, fig3, fileinfo
 
 def drawGraph(opt):
     data = mainfile.copy()
     print(mainfile.shape)
     # time domain
     if opt == 0:
-        # data = data[:4096]
+        data = data[:4096]
         fig = px.line(data, title = 'Time Domain')
     # frquence domain
     elif opt == 1:
@@ -264,7 +311,14 @@ def render_content(tab):
     elif tab == 'tab-2':
         return page2
 
+def outlier_detect(data):
+    data = data.copy()
+    IF = IsolationForest()
+    IF.fit(data.reshape(-1,1))
+    outlier = IF.predict(data.reshape(-1,1))
+    data = data[outlier==1]
 
+    return data
 
         
 
